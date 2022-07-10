c8e8f88a1cee0d1c47babd12184055aab70e7cf7
Lena <99120199+lenamin@users.noreply.github.com>
2022-07-10 17:32:14 +0900

    [FEAT] Table View 설정

--- DrinkWater/AlertListViewController.swift
+++ DrinkWater/AlertListViewController.swift
@@ -9,6 +9,8 @@ import UIKit
  
 class AlertListViewController: UITableViewController {
     
+    var alertList: [Alert] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,6 +18,36 @@ class AlertListViewController: UITableViewController {
         tableView.register(nibName, forCellReuseIdentifier: "AlertListCell")
     }
     @IBAction func addAlertButtonAction(_ sender: UIBarButtonItem) {
+    }
+    
+}
+
+extension AlertListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alertList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "🫙 물 마실 시간"
+        default:
+            return nil
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AlertListCell", for: indexPath) as? AlertListCell else { return UITableViewCell() }
+        
+        cell.alertSwitch.isOn = alertList[indexPath.row].isOn
+        cell.timeLabel.text = alertList[indexPath.row].time
+        cell.meridiemLabel.text = alertList[indexPath.row].meridiem
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
     
 }
